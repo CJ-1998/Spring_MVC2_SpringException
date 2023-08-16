5a77a694e5210a004bb7432776165e9e0835433b
CJ <choijun0627@konkuk.ac.kr>
2023-08-16 21:24:26 +0900

    <feat> 스프링 MVC 2편 프로젝트 section 9
    HandlerExceptionResolver 이해
    
    HandlerExceptionResolver를 만들기 위해 MyHandlerExceptionResolver 생성
    다른 exception을 발생 시키기 위해 ApiExceptionController 수정
    ExceptionResolver를 등록하기 위해 WebConfig에 설정 추가

--- src/main/java/hello/exception/WebConfig.java
+++ src/main/java/hello/exception/WebConfig.java
@@ -2,16 +2,28 @@ package hello.exception;
 
 import hello.exception.filter.LogFilter;
 import hello.exception.interceptor.LogInterceptor;
+import hello.exception.resolver.MyHandlerExceptionResolver;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.HandlerExceptionResolver;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import javax.servlet.DispatcherType;
 import javax.servlet.Filter;
+import java.util.List;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
+
+    /**
+     * 기본 설정을 유지하면서 추가
+     */
+    @Override
+    public void extendHandlerExceptionResolvers(List<HandlerExceptionResolver>
+                                                        resolvers) {
+        resolvers.add(new MyHandlerExceptionResolver());
+    }
 
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
