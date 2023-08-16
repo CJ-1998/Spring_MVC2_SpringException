7ef7577d194309d412af271594a15ee51b207ae7
CJ <choijun0627@konkuk.ac.kr>
2023-08-16 18:33:22 +0900

    <feat> 스프링 MVC 2편 프로젝트 section 9
    스프링 부트가 제공하는 BasicErrorController로 api 예외 처리
    
    스프링부트가 제공하는 BasicErrorController로 api 예외 처리하기 위해
    WebServerCustomizer의 @Component 주석 처리
    BasicErrorController에서 accept에 따라서 html 제공하는 것과 json
    제공하는 것 다 만들어서 제공해준다

--- src/main/java/hello/exception/servlet/ErrorPageController.java
+++ src/main/java/hello/exception/servlet/ErrorPageController.java
@@ -41,15 +41,15 @@ public class ErrorPageController {
     @RequestMapping(value = "/error-page/500", produces = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<Map<String, Object>> errorPage500Api(HttpServletRequest request, HttpServletResponse response) {
         log.info("API errorPage 500");
-
+        
         Map<String, Object> result = new HashMap<>();
-
+        
         Exception ex = (Exception) request.getAttribute(ERROR_EXCEPTION);
         result.put("status", request.getAttribute(ERROR_STATUS_CODE));
         result.put("message", ex.getMessage());
-
-        Integer statusCode = (Integer) request.getAttribute(RequestDispatcher.ERROR_STATUS_CODE);
         
+        Integer statusCode = (Integer) request.getAttribute(RequestDispatcher.ERROR_STATUS_CODE);
+
         return new ResponseEntity(result, HttpStatus.valueOf(statusCode));
     }
 
