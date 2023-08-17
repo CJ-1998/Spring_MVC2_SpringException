7d30d37039c36ee87bf0ab63db929ee36761a0da
CJ <choijun0627@konkuk.ac.kr>
2023-08-18 01:02:44 +0900

    <feat> 스프링 MVC 2편 프로젝트 section 9
    @ControllerAdvice 사용
    
    ApiExceptionV2Controller에서 @ExceptionHandler로 예외 처리하는 부분 제거
    ExControllerAdvice 생성해서 @RestControllerAdvice 해서
    @ExceptionHandler로 예외 처리하는 것들 모아 놓고 여러 컨트롤러에
    공통적으로 예외 처리할 수 있음
    또한 예외 처리할 컨트롤러들을 지정할 수 도 있음

--- src/main/java/hello/exception/api/ApiExceptionV2Controller.java
+++ src/main/java/hello/exception/api/ApiExceptionV2Controller.java
@@ -12,28 +12,28 @@ import org.springframework.web.bind.annotation.*;
 @Slf4j
 @RestController
 public class ApiExceptionV2Controller {
-
-    @ResponseStatus(HttpStatus.BAD_REQUEST)
-    @ExceptionHandler(IllegalArgumentException.class)
-    public ErrorResult illegalExHandle(IllegalArgumentException e) {
-        log.error("[exceptionHandle] ex", e);
-        return new ErrorResult("BAD", e.getMessage());
-    }
-
-    @ExceptionHandler
-    public ResponseEntity<ErrorResult> userExHandle(UserException e) {
-        log.error("[exceptionHandle] ex", e);
-        ErrorResult errorResult = new ErrorResult("USER-EX", e.getMessage());
-        return new ResponseEntity<>(errorResult, HttpStatus.BAD_REQUEST);
-    }
-
-    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
-    @ExceptionHandler
-    public ErrorResult exHandle(Exception e) {
-        log.error("[exceptionHandle] ex", e);
-        return new ErrorResult("EX", "내부 오류");
-    }
-
+    
+//    @ResponseStatus(HttpStatus.BAD_REQUEST)
+//    @ExceptionHandler(IllegalArgumentException.class)
+//    public ErrorResult illegalExHandle(IllegalArgumentException e) {
+//        log.error("[exceptionHandle] ex", e);
+//        return new ErrorResult("BAD", e.getMessage());
+//    }
+//
+//    @ExceptionHandler
+//    public ResponseEntity<ErrorResult> userExHandle(UserException e) {
+//        log.error("[exceptionHandle] ex", e);
+//        ErrorResult errorResult = new ErrorResult("USER-EX", e.getMessage());
+//        return new ResponseEntity<>(errorResult, HttpStatus.BAD_REQUEST);
+//    }
+//
+//    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+//    @ExceptionHandler
+//    public ErrorResult exHandle(Exception e) {
+//        log.error("[exceptionHandle] ex", e);
+//        return new ErrorResult("EX", "내부 오류");
+//    }
+    
     @GetMapping("/api2/members/{id}")
     public MemberDto getMember(@PathVariable("id") String id) {
         if (id.equals("ex")) {
@@ -47,7 +47,7 @@ public class ApiExceptionV2Controller {
         }
         return new MemberDto(id, "hello " + id);
     }
-    
+
     @Data
     @AllArgsConstructor
     static class MemberDto {
